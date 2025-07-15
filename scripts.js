ed011c5f383cf66ee9d26e3568549630e744c686
Aliyah Zaizay <aliyahkzaizay@gmail.com>
2025-07-15 19:47:44 -0400

    worked on project page

--- scripts.js
+++ scripts.js
@@ -3,3 +3,12 @@
 if (window.location.hash) {
     history.replaceState(null, null, ' ');
   }
+
+
+/*Card Animation*/
+function focusCard(clickedCard) {
+    const allCards = document.querySelectorAll('.card');
+    allCards.forEach(card => card.classList.remove('focused'));
+    clickedCard.classList.add('focused');
+  }
+  
